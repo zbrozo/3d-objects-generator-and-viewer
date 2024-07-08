2213b53769444e9c308b57ae95bfcf4d74ae1547
Tomasz Zbrożek <tomasz.zbrozek@o2.pl>
2024-07-08 12:14:16 +0200

    Rotation unittests added

--- src/Rotation.hpp
+++ src/Rotation.hpp
@@ -1,9 +1,22 @@
 #include "Types.hpp"
+#include "Vertex3D.hpp"
+#include <type_traits>
+
+template <typename Derived, typename Base1, typename Base2>
+struct IsDerivedFromEither {
+  static constexpr bool value =
+    std::is_base_of_v<Base1, Derived>
+    || std::is_base_of_v<Base2, Derived>;
+};
 
 template<class T>
 class Rotation3D
 {
- public:
+  static_assert(IsDerivedFromEither<T, Vertex, Vector>::value,
+    "Rotation can use only Vertex or Vector based types"
+    );
+  
+public:
   
   T rotateX(T coord, int d);
   T rotateY(T coord, int d);
--- src/UnitTests.cpp
+++ src/UnitTests.cpp
@@ -12,6 +12,7 @@
 #include "ComponentFactories.hpp"
 #include "Object3D.hpp"
 #include "Params.hpp"
+#include "Rotation.hpp"
 
 BOOST_AUTO_TEST_SUITE(Basic_Suite)
 
@@ -61,6 +62,72 @@ BOOST_AUTO_TEST_CASE(vector_compare_test)
   const Vector3D<int> vector1 {{0,0,0}, {10, 10, 10}};
   const Vector3D<int> vector2 {{0,0,0}, {10, 10, 10}};
   BOOST_CHECK_EQUAL(true, vector1 == vector2);
+}
+
+BOOST_AUTO_TEST_CASE(vertex_rotationx_test)
+{
+  Vertex3D<int> vertex {0, 50, 0};
+  Rotation3D<Vertex> rotation;
+  
+  const int scale = 10; 
+  
+  const auto vertex1 = rotation.rotateX(vertex * scale, 90) / scale;
+  const auto vertex2 = rotation.rotateX(vertex * scale, 180) / scale;
+  const auto vertex3 = rotation.rotateX(vertex * scale, 270) / scale;
+  const auto vertex4 = rotation.rotateX(vertex * scale, 360) / scale;
+  
+  BOOST_CHECK_EQUAL(0, vertex1.getY());
+  BOOST_CHECK_EQUAL(50, vertex1.getZ());
+  BOOST_CHECK_EQUAL(-50, vertex2.getY());
+  BOOST_CHECK_EQUAL(0, vertex2.getZ());
+  BOOST_CHECK_EQUAL(0, vertex3.getY());
+  BOOST_CHECK_EQUAL(-50, vertex3.getZ());
+  BOOST_CHECK_EQUAL(50, vertex4.getY());
+  BOOST_CHECK_EQUAL(0, vertex4.getZ());
+}
+
+BOOST_AUTO_TEST_CASE(vertex_rotationy_test)
+{
+  Vertex3D<int> vertex {50, 0, 0};
+  Rotation3D<Vertex> rotation;
+  
+  const int scale = 10; 
+  
+  const auto vertex1 = rotation.rotateY(vertex * scale, 90) / scale;
+  const auto vertex2 = rotation.rotateY(vertex * scale, 180) / scale;
+  const auto vertex3 = rotation.rotateY(vertex * scale, 270) / scale;
+  const auto vertex4 = rotation.rotateY(vertex * scale, 360) / scale;
+  
+  BOOST_CHECK_EQUAL(0, vertex1.getX());
+  BOOST_CHECK_EQUAL(-50, vertex1.getZ());
+  BOOST_CHECK_EQUAL(-50, vertex2.getX());
+  BOOST_CHECK_EQUAL(0, vertex2.getZ());
+  BOOST_CHECK_EQUAL(0, vertex3.getX());
+  BOOST_CHECK_EQUAL(50, vertex3.getZ());
+  BOOST_CHECK_EQUAL(50, vertex4.getX());
+  BOOST_CHECK_EQUAL(0, vertex4.getZ());
+}
+
+BOOST_AUTO_TEST_CASE(vertex_rotationz_test)
+{
+  Vertex3D<int> vertex {0, 50, 0};
+  Rotation3D<Vertex> rotation;
+  
+  const int scale = 10; 
+  
+  const auto vertex1 = rotation.rotateZ(vertex * scale, 90) / scale;
+  const auto vertex2 = rotation.rotateZ(vertex * scale, 180) / scale;
+  const auto vertex3 = rotation.rotateZ(vertex * scale, 270) / scale;
+  const auto vertex4 = rotation.rotateZ(vertex * scale, 360) / scale;
+  
+  BOOST_CHECK_EQUAL(-50, vertex1.getX());
+  BOOST_CHECK_EQUAL(0, vertex1.getY());
+  BOOST_CHECK_EQUAL(0, vertex2.getX());
+  BOOST_CHECK_EQUAL(-50, vertex2.getY());
+  BOOST_CHECK_EQUAL(50, vertex3.getX());
+  BOOST_CHECK_EQUAL(0, vertex3.getY());
+  BOOST_CHECK_EQUAL(0, vertex4.getX());
+  BOOST_CHECK_EQUAL(50, vertex4.getY());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
