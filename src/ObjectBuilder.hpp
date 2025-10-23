36d40fb15fe27bdc19bc34a9c6c5483023020cda
Tomasz Zbrożek <tomasz.zbrozek@o2.pl>
2025-10-23 20:32:07 +0200

    refactored

---
+++ src/ObjectBuilder.hpp
@@ -0,0 +1,43 @@
+#pragma once
+#include "Object3D.hpp"
+
+class ObjectBuilder : public Object3D
+{
+public:
+
+  ObjectBuilder() : Object3D("")
+  {
+  }
+
+  ~ObjectBuilder() = default;
+
+  ObjectBuilder(const ObjectBuilder&) = delete;
+  ObjectBuilder(ObjectBuilder&&) = delete;
+  ObjectBuilder& operator=(const ObjectBuilder&) = delete;
+  ObjectBuilder& operator=(ObjectBuilder&&) = delete;
+
+  ObjectBuilder(const Object3D& object) :
+    Object3D(object)
+  {
+  }
+
+  void SetVertices(const Vertices& vertices)
+  {
+    mVertices = vertices;
+  }
+
+  void SetFaces(const Faces& faces)
+  {
+    mFaces = faces;
+  }
+
+  void SetNormalVectorsInVertices(const Vectors& vectors)
+  {
+    mNormalVectorsInVertices = vectors;
+  }
+
+  void SetNormalVectorsInFaces(const Vectors& vectors)
+  {
+    mNormalVectorsInFaces = vectors;
+  }
+};
